fa8b3561ae67bc5b368c4e5852ccc9249ef243bf
Andrew R. Johnson <andrew.johnson@arjohnsonau.com>
2025-06-09 15:30:02 +0000

    version 1.8.0

--- src/libquickjs.c
+++ src/libquickjs.c
@@ -1,19 +1,5 @@
-#include <stdio.h>
-
-void Rf_error(const char* fmt, ...);
-
-void Rf_abort(void) {
-  Rf_error("Aborted");
-}
-void Rf_exit(int status) {
-  Rf_error("Exited with status %d", status);
-}
-
-#define abort Rf_abort
-#define exit Rf_exit
-
 #include "quickjs/cutils.c"
-#include "quickjs/libbf.c"
+#include "quickjs/xsum.c"
 #include "quickjs/libregexp.c"
 #include "quickjs/libunicode.c"
 #include "quickjs/quickjs.c"
